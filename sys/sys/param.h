df83c0a5a7a5f1b4212dfc736a55490c8cc84e02
KONTROL Tecnologia <guzzy@uol.com.br>
2026-01-06 22:19:40 -0300

    Set version numbers for FreeBSD 15.0-RELEASE

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -40,7 +40,7 @@
 #include <sys/_null.h>
 #include <sys/_param.h>
 
-#define	BSD	1500063		/* System version (year & month). */
+#define	BSD	1500000		/* System version (year & month). */
 #define BSD4_3	1
 #define BSD4_4	1
 
@@ -74,7 +74,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1600001
+#define __FreeBSD_version 1500000
 
 /*
  * __FreeBSD_kernel__ indicates that this system uses the kernel of FreeBSD,
